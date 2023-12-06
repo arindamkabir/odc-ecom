7f76b1b7a60046f307b6654daff6df6fc26cdbc0
Arindam Kabir <arindamkabir@gmail.com>
2023-12-06 18:27:31 +0600

    feat: installed react tooltip

---
+++ components/cart/drawer/CartDrawer.tsx
@@ -0,0 +1,130 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import useStore from '@/store/store'
+import CartDrawerProduct from './Product'
+import PrimaryButton from '../../common/PrimaryButton'
+
+const products = [
+    {
+        id: 1,
+        name: 'Throwback Hip Bag',
+        href: '#',
+        color: 'Salmon',
+        price: '$90.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+    },
+    {
+        id: 2,
+        name: 'Medium Stuff Satchel',
+        href: '#',
+        color: 'Blue',
+        price: '$32.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt:
+            'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+    },
+    // More products...
+]
+
+export default function CartDrawer() {
+    const cart = useStore(state => state.cart);
+    const cartOpen = useStore(state => state.cartOpen);
+    const setCartOpen = useStore(state => state.setCartOpen);
+
+    return (
+        <Transition.Root show={cartOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => setCartOpen(false)}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-500"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-hidden">
+                    <div className="absolute inset-0 overflow-hidden">
+                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                                enterFrom="translate-x-full"
+                                enterTo="translate-x-0"
+                                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                                leaveFrom="translate-x-0"
+                                leaveTo="translate-x-full"
+                            >
+                                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                                        <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                                            <div className="flex items-start justify-between">
+                                                <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                                                <div className="ml-3 flex h-7 items-center">
+                                                    <button
+                                                        type="button"
+                                                        className="relative -m-2 p-2 text-black"
+                                                        onClick={() => setCartOpen(false)}
+                                                    >
+                                                        <span className="absolute -inset-0.5" />
+                                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                            <div className="mt-8">
+                                                <div className="flow-root">
+                                                    <ul role="list" className="-my-6 divide-y divide-gray-200">
+                                                        {cart.map((product) => (
+                                                            <CartDrawerProduct
+                                                                key={`cart-drawer-product-${product.slug}-${product.stock.id}`}
+                                                                product={product}
+                                                            />
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                                            <div className="flex justify-between text-base font-medium text-gray-900">
+                                                <p>Subtotal</p>
+                                                <p>$262.00</p>
+                                            </div>
+                                            {/* <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p> */}
+                                            <div className="mt-6">
+                                                <PrimaryButton className='!block !max-w-full'>
+                                                    Checkout
+                                                </PrimaryButton>
+                                            </div>
+                                            <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                                                <p>
+                                                    or
+                                                    <button
+                                                        type="button"
+                                                        className="font-medium text-black pl-1"
+                                                        onClick={() => setCartOpen(false)}
+                                                    >
+                                                        Continue Shopping
+                                                        <span aria-hidden="true"> &rarr;</span>
+                                                    </button>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
